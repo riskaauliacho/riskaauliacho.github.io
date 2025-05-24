dc171640ae50d79d57e9e48d704dcba774abbdf7
riskaauliacho <53550385+riskaauliacho@users.noreply.github.com>
2025-05-24 12:52:13 +0700

    Delete Arkademy Batch 12 - 1 directory

--- Arkademy Batch 12 - 1/No_7/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'koneksi.php';
-$id = $_GET['id'];
-$query = "DELETE FROM nama WHERE nama.id = '$id'";
-if (mysqli_query($conn, $query)) {
-    header("location:index.php");
-} else {
-    echo "Gagal";
-}
